ad935252a80c9646c37139aba0f9fb710b96230d
aliiasad <alii.asad0519@gmail.com>
2026-03-18 01:34:44 +0500

    HomeWork 3 Completed

--- HomeWork-3.cpp
+++ HomeWork-3.cpp
@@ -150,7 +150,53 @@ bool MyString::operator<(MyString dummy) {
 }
 
 int main()  {
+    MyString str1, str2, str3, str4; //Default constructor will make a string of 
+    //lenght 0 but having null character (only) i.e. empty string
+    if(!str1)   {
+    cout<<"String 1 is Empty.\n";
+    cout<<"Str 1 = "<<str1<<endl<<endl<<endl;
+    }
 
+    cout << "Enter String 1:\t";
+    cin >> str1;
+    cout << "Enter String 2:\t";
+    cin >> str2;
+    cout << "\n\n\nUser Entered:\n";
+    cout << "String 1 = " << str1 << endl;
+    cout << "String 2 = " << str2 << endl<<endl<<endl;
+    //What is following code testing?
+    cout<<"Before str1 = str1; str1 = "<<str1<<endl;
+    str1 = str1;
+    cout<<"After str1 = str1, str1 = "<<str1<<endl<<endl<<endl;
+    cout<<"Before str4 = str3 = str1+str2\n";
+    cout<<"str1 = "<<str1<<endl;
+    cout<<"str2 = "<<str2<<endl;
+    cout<<"str3 = "<<str3<<endl;
+    cout<<"str4 = "<<str4<<endl;
+    str4 = str3 = str1+str2;
+    cout<<"\n\n\nAfter str4 = str3 = str1+str2\n";
+    cout<<"str1 = "<<str1<<endl;
+    cout<<"str2 = "<<str2<<endl;
+    cout<<"str3 = "<<str3<<endl;
+    cout<<"str4 = "<<str4<<endl;
+    cout<<"\n\n\nEnter String 3:\t";
+    cin >> str3;
+    cout<<"\n\n\nEnter String 4:\t";
+    cin >> str4;
+    cout<<"\n\n\nstr3 = "<<str3<<endl;
+    cout<<"str4 = "<<str4<<endl;
+    if(str3 < str4)
+    cout<<"String 3 is Less than String 4.\n";
+    else
+    cout<<"String 3 is NOT Less than String 4.\n";
+    MyString str5 = str1 + str2;
+    cout << "\n\n\nStr5:\t" << str5 << endl;
+    cout << "Str5[7]:\t" << str5[7] << endl; //Function Call: str5.operator[](7).
+    str5[7] = '$';
+    cout << "\n\nStr5:\t" << str5 << endl;
+    cout << "\n\n\nstr5(5, 10):\t" << str5(5, 10) << endl;// Substring of lenght 10 
+    //starting from index 5 . Function Call str5.operator()(5,10) Let the returned MyString or 
+    //char* leak
     return 0;
 }
 
@@ -168,6 +214,7 @@ char* dynamic_cpy(const char* src)  {
         dest[i] = src[i];
         i++;
     }
+    dest[i] = '\0';
     return dest;
 }
 
